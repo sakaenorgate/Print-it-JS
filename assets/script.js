70d0527054a4fb9e1fb65ea2e5cee0c1ccbdc41f
Sakae Norgate <113297224+sakaenorgate@users.noreply.github.com>
2023-08-23 23:08:03 +0200

    Add slides

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,37 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let numero = 0;
+
+const flecheLeft = document.querySelector(".arrow_left");
+flecheLeft.addEventListener("click", function() { ChangeSlide(-1);
+	console.log(flecheLeft);
+});
+
+const flecheRight = document.querySelector(".arrow_right");
+flecheRight.addEventListener("click", function() { ChangeSlide(1);
+	console.log(flecheRight);
+});
+
+console.log(slides.length);
+
+function ChangeSlide(sens) {
+	numero = numero + sens;
+	if (numero > slides.length -1)
+		numero = 0;
+	if (numero < 0)
+		numero = slides.length -1;
+
+	document.querySelector(".banner-img").src = './assets/images/slideshow/' + slides[numero]['image'];
+	document.getElementById("text").innerHTML = slides[numero]['tagLine'];
+
+	console.log(numero);
+}
+
+const codeDot = document.querySelector(".dots").innerHTML = '<p class="dot"></p>'.repeat(slides.length);
+{
+	console.log(codeDot);
+}
+
+
