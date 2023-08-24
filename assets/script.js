b584ac1e57762a93ca8a43eb2220f769d7a994d8
Sakae Norgate <113297224+sakaenorgate@users.noreply.github.com>
2023-08-24 22:43:41 +0200

    Add dots

--- assets/script.js
+++ assets/script.js
@@ -22,12 +22,12 @@ let numero = 0;
 const flecheLeft = document.querySelector(".arrow_left");
 flecheLeft.addEventListener("click", function() { ChangeSlide(-1);
 	console.log(flecheLeft);
-});
+})
 
 const flecheRight = document.querySelector(".arrow_right");
 flecheRight.addEventListener("click", function() { ChangeSlide(1);
 	console.log(flecheRight);
-});
+})
 
 console.log(slides.length);
 
@@ -41,12 +41,53 @@ function ChangeSlide(sens) {
 	document.querySelector(".banner-img").src = './assets/images/slideshow/' + slides[numero]['image'];
 	document.getElementById("text").innerHTML = slides[numero]['tagLine'];
 
+	showDotSelected();
+
 	console.log(numero);
 }
 
-const codeDot = document.querySelector(".dots").innerHTML = '<p class="dot"></p>'.repeat(slides.length);
-{
+const codeDot = document.querySelector(".dots").innerHTML = '<p class="dot"></p> '.repeat(slides.length);
+var dots = document.getElementsByClassName("dot");
+
+showDotSelected();
+function showDotSelected() {
+	var i;
+	
+	if (numero +1> slides.length) {numero = 0}
+	console.log(numero);
+
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" dot_selected", "");	
+	}
+	dots[numero].className += " dot_selected";
+	
+	console.log(dots[0]);
+	console.log(dots[1]);
+	console.log(dots[2]);
+	console.log(dots[3]);
 	console.log(codeDot);
+	console.log(i+"!!!");
+
 }
+
+
+
+
+
+
+
+
+
+// console.log(codeDot);
+
+// let n = 0;
+// while (n < slides.length) {
+// 	if (n == numero)
+// 	// codeDot.n.innerText = '<p class="dot dot_selected"></p>';
+// 	// document.getElementById(".dots").innerHTML = '<p class="dot dot_selected"></p>';
+
+// 	console.log(n);
+// 	n++;
+// };
 
 
